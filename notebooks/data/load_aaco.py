ee3a2431c869c4e9384985a55ecba290995b1ec9
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-02-16 22:53:29 -0500

    print data shape

--- notebooks/data/load_aaco.py
+++ notebooks/data/load_aaco.py
@@ -7,17 +7,22 @@ import mydatasets.aaco
 tcube, vcube, tstcube = mydatasets.aaco.load_aaco_data(
     "cube_20_0.3", to_normalize=False
 )
+print(tcube["xs"].shape)
 
 # %%
 tgas, vgas, tstgas = mydatasets.aaco.load_aaco_data("gas")
+print(tgas["xs"].shape)
 
 # %%
 tgrid, vgrid, tstgrid = mydatasets.aaco.load_aaco_data("grid_data")
+print(tgrid["xs"].shape)
 
 # %%
 tmnist, vmnist, tstmnist = mydatasets.aaco.load_aaco_data("mnist")
+print(tmnist["xs"].shape)
 
 # %%
 tbg5, vbg5, tstbg5 = mydatasets.aaco.load_aaco_data("big5_C_cls", to_normalize=True)
+print(tbg5["xs"].shape)
 
 # %%
