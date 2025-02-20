9ceb85ade042d5bfdb58a1b0aeda02e2c983b473
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-02-19 21:29:50 -0500

    add run_one_episode_all_obsd

--- notebooks/make_tmpls/sltknn_fxmrgreedy.py
+++ notebooks/make_tmpls/sltknn_fxmrgreedy.py
@@ -936,6 +936,49 @@ def run_one_episode(
     return pyhats[0], fobsd_l, fcomb
 
 
+def run_one_episode_all_obsd(
+    x: th.Tensor,
+    classifier: mymodels.classifiers.SubsetFeatureClassifier,
+    cost_est: Callable[[th.Tensor], th.Tensor],
+    init_fidx: int,
+    allfcombs_l: list[tuple[int, ...]],
+    plf: pl.Fabric,
+) -> tuple[th.Tensor, list[int], tuple[int, ...]]:
+    if isinstance(cost_est, th.nn.Module):
+        cost_est.eval().to(device=plf.device)
+    fobsd_l: list[int] = [init_fidx]
+    fcomb: tuple[int, ...] | None = None
+    for _ in itrtls.count():
+        # make feature bit mask
+        _m: th.Tensor = th.zeros_like(x)
+        _m[fobsd_l] = 1
+        # forward prop. cost est.
+        # (1, 2 * n_covs)
+        _inps: th.Tensor = th.cat((x, _m))[None, :]
+        # (1, n_tmpls)
+        _costs: th.Tensor = cost_est(_inps.to(device=plf.device)).to(device="cpu")
+        _fcomb_idx: int = int(th.argmin(_costs[0]).item())
+        _tmpl_fcomb: tuple[int, ...] = allfcombs_l[_fcomb_idx]
+        # ident. unacquired features
+        _tmp_fcomb: list[int] = [fidx for fidx in _tmpl_fcomb if fidx not in fobsd_l]
+        if len(_tmp_fcomb) == 0:
+            fcomb = _tmpl_fcomb
+            break
+        # randomly choose a feature to acquire
+        _tmp_fcomb_idx = int(th.randint(0, len(_tmp_fcomb), size=(1,)).item())
+        # add acquired feature to fcomb
+        fobsd_l.append(_tmp_fcomb[_tmp_fcomb_idx])
+        # terminate acq. if all features in template has been satisfied
+        if all([fidx in fobsd_l for fidx in _tmpl_fcomb]):
+            fcomb = _tmpl_fcomb
+            break
+    assert fcomb is not None
+    acts: th.Tensor = th.zeros((1, x.shape[0]), dtype=th.long, device=x.device)
+    acts[0, fobsd_l] = 1
+    pyhats: th.Tensor = classifier.predict_proba(x[None, :], acts)
+    return pyhats[0], fobsd_l, fcomb
+
+
 # %%
 # NOTE cube
 # data_name: str = "cube_20_0.3"
@@ -1107,5 +1150,46 @@ metrics_d.update(
 )
 print(pd.Series(metrics_d))
 
+
+#%%
+vclassifier = mymodels.classifiers.SubsetFeatureXGBClassifier(
+    xs_train=extdata["xs"].numpy(),
+    ys_train=extdata["ys"].numpy(),
+    xgbc_kwargs={"n_estimators": 40},
+)
+metrics_func.reset()
+snfobsd_l: list[int] = list()
+snfcomb_l: list[int] = list()
+for _data in vdata:
+    _pyhat, _fobsd_l, _fcomb = run_one_episode_all_obsd(
+        x=_data["xs"],
+        classifier=vclassifier,
+        cost_est=lambda x: knn_cost_est(
+            x,
+            lmbda=lmbda,
+            tinps=tdata["xs"],
+            tcels=tpcomp["cels"],
+            tmpls=tmpls,
+            n_neighs=2,
+            p=2,
+        ),
+        init_fidx=init_fidx,
+        allfcombs_l=feature_masks_to_feature_combs(tmpls),
+        plf=plf,
+    )
+    snfobsd_l.append(len(_fobsd_l))
+    snfcomb_l.append(len(_fcomb))
+    metrics_func.update(
+        _pyhat[None, :].to(device="cpu"), _data["ys"][None].to(device="cpu")
+    )
+metrics_d: dict[str, float] = {k: v.item() for k, v in metrics_func.compute().items()}
+metrics_func.reset()
+metrics_d.update(
+    {
+        "init_fidx": init_fidx,
+        "feature used & observed": th.mean(th.as_tensor(snfobsd_l, dtype=th.float32)).item(),
+    }
+)
+print(pd.Series(metrics_d))
 
 # %%
