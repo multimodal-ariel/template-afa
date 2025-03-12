337eca1a8b607e68cacafdd645029c5f4a4f96f2
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-03-12 14:09:42 -0400

    update train_arbitrary_subset_cloassifiers

--- notebooks/aco/train_arbitrary_subset_classifier_big5.py
+++ notebooks/aco/train_arbitrary_subset_classifier_big5.py
@@ -4,8 +4,11 @@ import numpy as np
 import torch as th
 import xgboost as xgbst
 
+#%%
+th.autograd.set_grad_enabled(False)
+
 # %%
-tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data("cube_20_0.3")
+tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data("big5_C_cls")
 
 # %%
 # ### Read in Cube data
@@ -20,23 +23,26 @@ tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data("cube_20_0.3")
 # cube_val_y = th.nn.functional.one_hot(th.from_numpy(cube_val[1]).long())
 
 # %%
-### Read in Cube data
-cube_train_X = tdata["xs"]
-cube_train_y = th.nn.functional.one_hot(tdata["ys"])
+### Read in data
+txs: th.Tensor = tdata["xs"]
+tys: th.Tensor = th.nn.functional.one_hot(tdata["ys"])
 
-cube_val_X = vdata["xs"]
-cube_val_y = th.nn.functional.one_hot(vdata["ys"])
+vxs: th.Tensor = vdata["xs"]
+vys: th.Tensor = th.nn.functional.one_hot(vdata["ys"])
+
+# %%
+n_covs: int = txs.shape[1]
 
 # %%
 # Generate 10,000,000 random masks
 np.random.seed(29)
 
-Rall = 10000000
+Rall = 3000
 
 Ball = np.concatenate(
     [
         np.sum(
-            np.random.permutation(np.eye(20))[:, : np.random.randint(20)],
+            np.random.permutation(np.eye(n_covs))[:, : np.random.randint(n_covs)],
             1,
             keepdims=True,
         )
@@ -44,18 +50,30 @@ Ball = np.concatenate(
     ],
     1,
 )
+Ball_t = th.as_tensor(Ball, dtype=th.float32)
 
 # %%
 # Set each 7th variable (index 6) each to 1 (only consider masks that select feature 7
-Ball[:, 6] = 1.0
+# Ball[:, 6] = 1.0
 
 # %%
 # Train the classifier in these 10,000,000 random masks
-data_x_train = np.hstack(
-    (cube_train_X.repeat(1000, 1).detach().numpy() * Ball.T - (1 - Ball.T) * 10, Ball.T)
+data_x_train = (
+    th.cat(
+        [
+            txs[:, None, :] * Ball_t.T[None, :, :] - (1 - Ball_t.T[None, :, :]) * 10,
+            Ball_t.T[None, :, :].expand(len(txs), -1, -1),
+        ],
+        dim=2,
+    )
+    .flatten(0, 1)
+    .numpy()
 )
-data_y_train = cube_train_y.repeat(1000, 1).argmax(dim=1).detach().numpy()
-
+data_y_train = tys[:, None, :].expand(-1, Rall, -1).flatten(0, 1).argmax(dim=1).numpy()
+# data_x_train = np.hstack(
+#     (txs.repeat(1000, 1).detach().numpy() * Ball.T - (1 - Ball.T) * 10, Ball.T)
+# )
+# data_y_train = tys.repeat(1000, 1).argmax(dim=1).detach().numpy()
 
 # %%
 model_xgb_arb = xgbst.XGBClassifier(
@@ -65,8 +83,7 @@ model_xgb_arb.fit(data_x_train, data_y_train)
 
 # %%
 # Save the model
-model_xgb_arb.save_model("cube_xgb_classifier_arb_subsets.json")
-
+model_xgb_arb.save_model("big5_xgb_classifier_arb_subsets.json")
 
 # %%
 # Print
