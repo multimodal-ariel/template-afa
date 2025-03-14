d7880d4b869730ad3633529f3d3106317fbb3fbe
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-03-14 01:41:46 -0400

    add get project root dir utility function

--- libs/internal/mylib/utils.py
+++ libs/internal/mylib/utils.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import os
 from typing import Any, Optional
 
 
@@ -5,3 +8,7 @@ def add_prefix_to_dict(
     d: dict[str, Any], prefix: str, sep: Optional[str] = "/"
 ) -> dict[str, Any]:
     return {f"{prefix}{sep}{k}": v for k, v in d.items()}
+
+
+def get_project_root_dir() -> str:
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
