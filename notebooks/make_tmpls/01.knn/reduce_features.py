dd7fbbac1f0b2b9ec5902929c6079dae41744fad
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-03-13 23:15:44 -0400

    notebook/01knn/reduce_features use tafalib

--- notebooks/make_tmpls/01.knn/reduce_features.py
+++ notebooks/make_tmpls/01.knn/reduce_features.py
@@ -5,85 +5,74 @@ import os
 from typing import Optional
 
 import _classifiers
-import _tmplfns
 import lightning as pl
 import lightning.fabric.loggers as plf_loggers
 import mydatasets.aaco
 import mymodels.classifiers
-import mymodels.nn
-import mymodels.protocols
 import pandas as pd
+import tafalib.functional
+import tafalib.makers.templates
+import tafalib.utils
 import tensordict as thd
 import torch as th
 import torchmetrics as thm
 
 # %%
 # NOTE cube
-# data_name: str = "cube_20_0.3"
-# max_tdata: Optional[int] = None
-# tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data(data_name, to_normalize=False)
-# n_covs: int = tdata["xs"].shape[1]
-# n_labels: int = len(th.unique(tdata["ys"]))
-# classifier = SubsetFeatureNaiveBayes(
-#     0.3, xs_train=tdata["xs"].numpy(), ys_train=tdata["ys"].numpy()
-# )
-# metrics_func = thm.MetricCollection(
-#     {
-#         "acc": thm.Accuracy(task="multiclass", num_classes=n_labels),
-#         "precision": thm.Precision(task="multiclass", num_classes=n_labels),
-#         "recall": thm.Recall(task="multiclass", num_classes=n_labels),
-#         "f1-score": thm.F1Score(task="multiclass", num_classes=n_labels),
-#         "auroc": thm.AUROC(task="multiclass", num_classes=n_labels),
-#     }
-# )
-# init_fidx: int = 6
-# n_tmpls_targ: int = 64
-# n_cands_targ: int = 10_000
-# min_features_targ: int = 1
-# max_features_targ: Optional[int] = None
-# min_features_init: int = 10
-# feature_decrement: int = 2
-# lmbda: float = 0.3
-# bsz: int = 1024
-
-# %%
-# NOTE big5
-data_name: str = "big5_C_cls"
-_tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data(data_name, to_normalize=False)
-n_covs: int = _tdata["xs"].shape[1]
-n_labels: int = len(th.unique(_tdata["ys"]))
-max_tdata: Optional[int] = 8192
-_tdata_shuffle_idxs = th.randperm(len(_tdata))
-tdata = _tdata[_tdata_shuffle_idxs[: len(_tdata) // 2]]
-extdata = _tdata[_tdata_shuffle_idxs[len(_tdata) // 2 :]]
-tclassifier = _classifiers.SubsetFeatureConcatXGBClassifier(
-    xs_train=extdata["xs"].numpy(),
-    ys_train=extdata["ys"].numpy(),
-    xgb_kwargs={"n_estimators": 40},
-    fraction_training_data_per_split=1.0,
-    n_splits=64,
-    n_tmpl_per_instance=4,
+data_name: str = "cube_20_0.3"
+max_tdata: Optional[int] = None
+tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data(data_name, to_normalize=False)
+n_covs: int = tdata["xs"].shape[1]
+n_labels: int = len(th.unique(tdata["ys"]))
+tclassifier = _classifiers.SubsetFeatureNaiveBayes(
+    0.3, xs_train=tdata["xs"].numpy(), ys_train=tdata["ys"].numpy()
 )
-metrics_func = thm.MetricCollection(
-    {
-        "acc": thm.Accuracy(task="multiclass", num_classes=n_labels),
-        "precision": thm.Precision(task="multiclass", num_classes=n_labels),
-        "recall": thm.Recall(task="multiclass", num_classes=n_labels),
-        "f1-score": thm.F1Score(task="multiclass", num_classes=n_labels),
-        "auroc": thm.AUROC(task="multiclass", num_classes=n_labels),
-    }
-)
-init_fidx: int = 35
-n_tmpls_targ: int = 128
+vclassifier = tclassifier
+init_fidx: int = 6
+n_tmpls_targ: int = 64
 n_cands_targ: int = 10_000
-lmbda: float = 0.075
 min_features_targ: int = 1
 max_features_targ: Optional[int] = None
 min_features_init: int = 10
-n_rounds: int = 3
 feature_decrement: int = 2
 use_feature_importance_sampling: bool = True
-bsz: int = 8192
+lmbda: float = 0.3
+bsz: int = 20_000_000
+
+# %%
+# NOTE big5
+# data_name: str = "big5_C_cls"
+# _tdata, vdata, tstdata = mydatasets.aaco.load_aaco_data(data_name, to_normalize=False)
+# n_covs: int = _tdata["xs"].shape[1]
+# n_labels: int = len(th.unique(_tdata["ys"]))
+# max_tdata: Optional[int] = 8192
+# _tdata_shuffle_idxs = th.randperm(len(_tdata))
+# tdata = _tdata[_tdata_shuffle_idxs[: len(_tdata) // 2]]
+# extdata = _tdata[_tdata_shuffle_idxs[len(_tdata) // 2 :]]
+# tclassifier = mymodels.classifiers.SubsetFeatureConcatXGBClassifier(
+#     xs_train=extdata["xs"].numpy(),
+#     ys_train=extdata["ys"].numpy(),
+#     xgb_kwargs={"n_estimators": 40},
+#     fraction_training_data_per_split=1.0,
+#     n_splits=64,
+#     n_tmpl_per_instance=4,
+# )
+# vclassifier = mymodels.classifiers.SubsetFeatureXGBClassifier(
+#     xs_train=extdata["xs"].numpy(),
+#     ys_train=extdata["ys"].numpy(),
+#     xgbc_kwargs={"n_estimators": 40},
+# )
+# init_fidx: int = 35
+# n_tmpls_targ: int = 128
+# n_cands_targ: int = 10_000
+# lmbda: float = 0.075
+# min_features_targ: int = 1
+# max_features_targ: Optional[int] = None
+# min_features_init: int = 10
+# n_rounds: int = 3
+# feature_decrement: int = 2
+# use_feature_importance_sampling: bool = True
+# bsz: int = 20_000_000
 
 # %%
 # configure logger and ckpt path
@@ -97,7 +86,7 @@ plf = pl.Fabric(loggers=[tfb_logger, csv_logger], accelerator="cpu")
 
 # %%
 if init_fidx is None:
-    init_fidx, bestfm = _tmplfns.identify_init_fidx(
+    init_fidx, bestfm = tafalib.makers.templates.identify_init_fidx(
         tdata=tdata,
         classifier=tclassifier,
         max_features=max_features_targ,
@@ -108,7 +97,7 @@ if init_fidx is None:
     )
 
 # %%
-tmpls = _tmplfns.make_templates_reduce_features(
+tmpls = tafalib.makers.templates.make_templates_reduce_features(
     tdata=tdata,
     max_tdata=max_tdata,
     classifier=tclassifier,
@@ -127,15 +116,19 @@ tmpls = _tmplfns.make_templates_reduce_features(
 )
 
 # %%
-tpcomp: thd.TensorDict = _tmplfns.precomp_rwds_for_tmpls(
-    tmpls=tmpls, data=tdata, classifier=tclassifier, lmbda=lmbda, bsz=bsz
+tpcomp: thd.TensorDict = tafalib.utils.precomp_rwds_for_tmpls(
+    tmpls=tmpls, data=tdata, classifier=tclassifier, lmbda=lmbda, bsz=bsz, plf=plf
 )
 
 # %%
-vclassifier = mymodels.classifiers.SubsetFeatureXGBClassifier(
-    xs_train=extdata["xs"].numpy(),
-    ys_train=extdata["ys"].numpy(),
-    xgbc_kwargs={"n_estimators": 40},
+metrics_func = thm.MetricCollection(
+    {
+        "acc": thm.Accuracy(task="multiclass", num_classes=n_labels),
+        "precision": thm.Precision(task="multiclass", num_classes=n_labels),
+        "recall": thm.Recall(task="multiclass", num_classes=n_labels),
+        "f1-score": thm.F1Score(task="multiclass", num_classes=n_labels),
+        "auroc": thm.AUROC(task="multiclass", num_classes=n_labels),
+    }
 )
 
 # %%
@@ -143,10 +136,10 @@ metrics_func.reset()
 snfobsd_l: list[int] = list()
 snfcomb_l: list[int] = list()
 for _data in vdata:
-    _pyhat, _fobsd_l, _fcomb = _tmplfns.run_one_episode(
+    _pyhat, _fobsd_l, _fcomb = tafalib.utils.run_one_episode(
         x=_data["xs"],
         classifier=vclassifier,
-        cost_est=lambda x: _tmplfns.knn_cost_est(
+        cost_est=lambda x: tafalib.functional.knn_cost_est(
             x,
             lmbda=lmbda,
             txs=tdata["xs"],
@@ -181,10 +174,10 @@ metrics_func.reset()
 snfobsd_l: list[int] = list()
 snfcomb_l: list[int] = list()
 for _data in vdata:
-    _pyhat, _fobsd_l, _fcomb = _tmplfns.run_one_episode_all_obsd(
+    _pyhat, _fobsd_l, _fcomb = tafalib.utils.run_one_episode_all_obsd(
         x=_data["xs"],
         classifier=vclassifier,
-        cost_est=lambda x: _tmplfns.knn_cost_est(
+        cost_est=lambda x: tafalib.functional.knn_cost_est(
             x,
             lmbda=lmbda,
             txs=tdata["xs"],
@@ -219,10 +212,10 @@ metrics_func.reset()
 snfobsd_l: list[int] = list()
 snfcomb_l: list[int] = list()
 for _data in vdata:
-    _pyhat, _fobsd_l, _fcomb = _tmplfns.run_one_episode_all_obsd(
+    _pyhat, _fobsd_l, _fcomb = tafalib.utils.run_one_episode_all_obsd(
         x=_data["xs"],
         classifier=tclassifier,
-        cost_est=lambda x: _tmplfns.knn_cost_est(
+        cost_est=lambda x: tafalib.functional.knn_cost_est(
             x,
             lmbda=lmbda,
             txs=tdata["xs"],
