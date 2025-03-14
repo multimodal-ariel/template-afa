61c03ec74259e817ea8d932358f8e6b0ecf9bfe3
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-03-14 01:41:59 -0400

    add make_concat_nnet_classifier_from_pretrain_run

--- experiments/make_template/_classifiers.py
+++ experiments/make_template/_classifiers.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
+import os
 
-import math
-from typing import Any, Optional
-
+import hydra as hd
+import mylib
+import mylib.utils
 import mymodels.classifiers
 import mymodels.nn
 import mymodels.protocols
 import numpy as np
 import torch as th
-import tqdm.auto as tqdm
-import xgboost as xgbst
+from omegaconf import OmegaConf
 
 
 class SubsetFeatureNaiveBayes(mymodels.classifiers.SubsetFeatureClassifier[None]):
@@ -72,86 +72,27 @@ class SubsetFeatureNaiveBayes(mymodels.classifiers.SubsetFeatureClassifier[None]
         return None
 
 
-class SubsetFeatureConcatXGBClassifier(
-    mymodels.classifiers.SubsetFeatureConcatClassifier[None]
-):
-    xgb_kwargs: dict[str, Any]
-    fraction_training_data_per_split: float
-    n_splits: int
-    n_tmpl_per_instance: int
-    rseed: Optional[int]
-
-    _models: list[xgbst.XGBClassifier]
-
-    def __init__(
-        self,
-        xs_train: np.ndarray,
-        ys_train: np.ndarray,
-        xgb_kwargs: dict[str, Any],
-        fraction_training_data_per_split: float,
-        n_splits: int,
-        n_tmpl_per_instance: int,
-        rseed: Optional[int] = None,
-    ):
-        super().__init__(n_experts_per_act=1, xs_train=xs_train, ys_train=ys_train)
-        self.xgb_kwargs = xgb_kwargs
-        self.fraction_training_data_per_split = fraction_training_data_per_split
-        self.n_splits = n_splits
-        self.n_tmpl_per_instance = n_tmpl_per_instance
-        self.rseed = rseed
-        self._models = [xgbst.XGBClassifier(**self.xgb_kwargs) for _ in range(n_splits)]
-
-    def fit_(self, acts_tmpls: th.Tensor) -> dict[str, float]:
-        txs: th.Tensor = th.as_tensor(self.xs_train, dtype=th.float32)
-        tys: th.Tensor = th.as_tensor(self.ys_train)
-        pbar = tqdm.tqdm(
-            self._models, desc="model rsplit", dynamic_ncols=True, leave=False
-        )
-        for _m in pbar:
-            _n_data: int = math.ceil(len(txs) * self.fraction_training_data_per_split)
-            _idxs: th.Tensor = th.randint(0, len(txs), size=(_n_data,), dtype=th.long)
-            # (_n_data, n_tmpl_per_instance, n_covs)
-            _xs: th.Tensor = txs[_idxs, None, :].expand(
-                -1, self.n_tmpl_per_instance, -1
-            )
-            _fms: th.Tensor = th.stack(
-                [
-                    acts_tmpls[
-                        th.multinomial(
-                            th.arange(0, len(acts_tmpls), dtype=th.float32),
-                            self.n_tmpl_per_instance,
-                        )
-                    ]
-                    for _ in range(len(_xs))
-                ]
-            )
-            # (_n_data, n_tmple_per_instance, 2 * n_covs)
-            _minps: th.Tensor = th.cat((_xs * _fms, _fms), dim=2)
-            # (_n_data, n_tmpl_per_instance)
-            _ys: th.Tensor = tys[_idxs, None].expand(-1, self.n_tmpl_per_instance)
-            _m.fit(_minps.flatten(0, 1).numpy(), _ys.flatten(0, 1).numpy())
-        pbar.close()
-        return dict()
-
-    def predict_proba(self, ctxs: th.Tensor, acts: th.Tensor) -> th.Tensor:
-        device: th.device = ctxs.device
-        ctxs = ctxs.to(device="cpu")
-        acts = acts.to(device="cpu")
-        # (n, n_covs * 2)
-        minps: th.Tensor = th.cat((ctxs * acts, acts), dim=1)
-        # (n, n_splits, n_labels)
-        pyhats: th.Tensor = th.stack(
-            [
-                th.as_tensor(
-                    _m.predict_proba(minps.numpy(force=True)), dtype=th.float32
-                )
-                for _m in self._models
-            ],
-            dim=1,
-        )
-        # (n, n_labels)
-        pyhats = th.mean(pyhats, dim=1).to(device=device)
-        return pyhats
-
-    def __getitem__(self, key: tuple[int, ...]) -> None:
-        return None
+def make_concat_nnet_classifier_from_pretrain_run(
+    run_p: str,
+    xs_train: np.ndarray,
+    ys_train: np.ndarray,
+    fit_kwargs: mymodels.classifiers.SubsetFeatureConcatNeuralNetClassifier._FitKwargs,
+) -> mymodels.classifiers.SubsetFeatureConcatNeuralNetClassifier:
+    # try to load file from project root if run_p does not exist
+    if not os.path.exists(run_p):
+        run_p = os.path.join(mylib.utils.get_project_root_dir(), run_p)
+    run_cfg = OmegaConf.load(os.path.join(run_p, ".hydra", "config.yaml"))
+    n_covs: int = xs_train.shape[1]
+    n_labels: int = len(np.unique(ys_train))
+    classifier = mymodels.classifiers.SubsetFeatureConcatNeuralNetClassifier.from_saved_state_dict(
+        nnet=hd.utils.instantiate(
+            run_cfg.nnet,
+            in_features=n_covs * 2,
+            out_features=n_labels,
+        ),
+        xs_train=xs_train,
+        ys_train=ys_train,
+        fit_kwargs=fit_kwargs,
+        state_dict_p=os.path.join(run_p, "classifier.pt"),
+    )
+    return classifier
