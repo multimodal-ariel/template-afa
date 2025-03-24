015bd7b5937ada42ed158b1e87a28465cb646048
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-03-24 13:17:23 -0400

    add dropout classifier experiment

---
+++ experiments/policy/dropout/classifier/train.py
@@ -0,0 +1,331 @@
+from __future__ import annotations
+
+import logging
+import os
+import traceback
+from dataclasses import dataclass
+from typing import Any, Optional, TypedDict
+
+import hydra as hd
+import lightning as pl
+import lightning.fabric.loggers as plf_loggers
+import mylib
+import mymodels
+import tafalib
+import tensordict as thd
+import torch as th
+import torchmetrics as thm
+import tqdm.auto as tqdm
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import OmegaConf
+
+
+@dataclass
+class MainConf:
+    mktmpl_exp: Optional[MakeTemplateExpConf]
+    mktmpl_run: Optional[str]
+    nnet: Any
+    nnet_tcfg: NeuralNetTrainConf
+    plf: Any
+
+
+@dataclass
+class MakeTemplateExpConf:
+    exp_p: str
+    run_id: int
+
+
+@dataclass
+class NeuralNetTrainConf:
+    opt: Any
+    n_fit_iter: int
+    bsz: int
+    save_ckpt_every_n_iter: int
+
+
+OmegaConf.register_new_resolver(
+    name="get_cls", resolver=lambda cls: hd.utils.get_class(cls)
+)
+
+
+class _TrainState(TypedDict):
+    nnet: th.nn.Module
+    opt: th.optim.Optimizer
+    fit_itr: int
+    opt_step: int
+
+
+@th.no_grad()
+def compile_selector_dataset(
+    tdata: thd.TensorDict, tpcomp: thd.TensorDict
+) -> thd.TensorDict:
+    # (n_data, n_covs)
+    xs: th.Tensor = tdata["xs"]
+    # (n_data, )
+    ys: th.Tensor = tdata["ys"]
+    # (n_data, n_tmpls, n_labels)
+    pyhats: th.Tensor = tpcomp["pyhats"]
+    # (n_data, n_tmpls)
+    cels: th.Tensor = tpcomp["cels"]
+    rwds: th.Tensor = tpcomp["rwds"]
+    # # (n_data, n_tmpls)
+    # slbls: th.Tensor = th.softmax(rwds / tau_rwd, dim=1)
+    # bundle tensors into tensordict
+    stdata = thd.TensorDict(
+        {
+            "xs": xs,
+            "ys": ys,
+            "pyhats": pyhats,
+            "cels": cels,
+            "rwds": rwds,
+        }
+    ).auto_batch_size_(1)
+    return stdata
+
+
+@th.no_grad()
+def _make_fit_bsinps_bstargs(
+    bstdata: thd.TensorDict, init_fidx: int, lmbda: float, tmpls: th.Tensor
+) -> tuple[th.Tensor, th.Tensor]:
+    bsz: int = len(bstdata)
+    n_tmpls: int = len(tmpls)
+    # (bsz, )
+    btmplidxs: th.Tensor = th.randint(0, n_tmpls, (bsz,))
+    # (bsz, n_covs)
+    bxs: th.Tensor = bstdata["xs"]
+    tmpls = tmpls.to(device=bxs.device)
+    bfms: th.Tensor = tmpls[btmplidxs]
+    n_covs: int = bxs.shape[1]
+    # randomly drop features
+    boms: th.Tensor = th.randint(0, 2, (bsz, n_covs), device=bxs.device)
+    boms = th.clamp(bfms - boms, 0.0, 1.0)
+    boms[:, init_fidx] = 1
+    # (bsz, 2 * n_covs)
+    bsinps: th.Tensor = th.cat((bxs * boms, boms), dim=1)
+    # (bsz, n_tmpls, n_covs)
+    bfms_avail: th.Tensor = th.clamp(tmpls[None, :, :] - boms[:, None, :], 0.0, 1.0)
+    # (bsz, n_tmpls)
+    bcels: th.Tensor = bstdata["cels"].to(device=bxs.device)
+    bcosts: th.Tensor = bcels + lmbda * th.sum(bfms_avail, dim=2)
+    # (bsz, )
+    bstargs: th.Tensor = th.argmin(bcosts, dim=1)
+    return bsinps, bstargs
+
+
+def _fit_iter_nnet_selector(
+    tstate: _TrainState,
+    bstdata: thd.TensorDict,
+    init_fidx: int,
+    tmpls: th.Tensor,
+    lmbda: float,
+    plf: pl.Fabric,
+) -> dict[str, float]:
+    nnet: th.nn.Module = tstate["nnet"].train().to(device=plf.device)
+    opt: th.optim.Optimizer = tstate["opt"]
+    # (bsz, 2 * n_covs) (bsz, )
+    bsinps, bstargs = _make_fit_bsinps_bstargs(
+        bstdata=bstdata, init_fidx=init_fidx, lmbda=lmbda, tmpls=tmpls
+    )
+    # (bsz, n_tmpls)
+    bsouts: th.Tensor = nnet(bsinps)
+    # compute selector loss
+    bslosses: th.Tensor = th.nn.functional.cross_entropy(
+        bsouts, bstargs, reduction="none"
+    )
+    bsloss: th.Tensor = th.mean(bslosses)
+    # update selector parameter
+    opt.zero_grad()
+    bsloss.backward()
+    opt.step()
+    # track metrics
+    bmetrics_d: dict[str, float] = {
+        "bsloss": bsloss.item(),
+    }
+    tstate["opt_step"] = tstate["opt_step"] + 1
+    return bmetrics_d
+
+
+def fit_nnet_selector(
+    tstate: _TrainState,
+    stdata: thd.TensorDict,
+    init_fidx: int,
+    lmbda: float,
+    tmpls: th.Tensor,
+    n_iter: int,
+    bsz: int,
+    plf: pl.Fabric,
+    ckpt_p: Optional[str] = None,
+    save_ckpt_every_n_iter: int = 1,
+):
+    pbar = tqdm.trange(n_iter, dynamic_ncols=True, leave=True)
+    for _itr in pbar:
+        bstdata: thd.TensorDict = stdata[th.randint(0, len(stdata), (bsz,))].to(
+            device=plf.device
+        )
+        # track metrics
+        bmetrics_d: dict[str, float] = _fit_iter_nnet_selector(
+            tstate=tstate,
+            bstdata=bstdata,
+            init_fidx=init_fidx,
+            lmbda=lmbda,
+            tmpls=tmpls,
+            plf=plf,
+        )
+        pbar.set_postfix(bmetrics_d)
+        plf.log_dict(
+            mylib.utils.add_prefix_to_dict(bmetrics_d, "train_warmup"),
+            step=tstate["opt_step"],
+        )
+        if ckpt_p is not None and (
+            (_itr % save_ckpt_every_n_iter) == 0 or (_itr + 1) == n_iter
+        ):
+            plf.save(os.path.join(ckpt_p, f"warmup_itr{_itr}.pt"), tstate)
+    pbar.close()
+    return tstate
+
+
+def _get_mktmpl_run_dir(cfg: MainConf) -> str:
+    if hasattr(cfg, "mktmpl_run") and cfg.mktmpl_run is not None:
+        return cfg.mktmpl_run
+    assert hasattr(cfg, "mktmpl_exp") and cfg.mktmpl_exp is not None
+    return os.path.join(cfg.mktmpl_exp.exp_p, str(cfg.mktmpl_exp.run_id))
+
+
+def main(cfg: MainConf):
+    # _delay_import()
+    output_dir: str = HydraConfig.get().runtime.output_dir
+    # get experiment dir
+    mktmpl_run_dir: str = _get_mktmpl_run_dir(cfg)
+    # load saved result from make_templates
+    tmpls: th.Tensor = th.load(
+        os.path.join(mylib.utils.get_project_root_dir(), mktmpl_run_dir, "tmpls.pt"),
+        weights_only=False,
+    )
+    tpcomp: thd.TensorDict = th.load(
+        os.path.join(mylib.utils.get_project_root_dir(), mktmpl_run_dir, "tpcomp.pt"),
+        weights_only=False,
+    )
+    mktmpl_cfg = OmegaConf.load(
+        os.path.join(
+            mylib.utils.get_project_root_dir(), mktmpl_run_dir, ".hydra", "config.yaml"
+        )
+    )
+    OmegaConf.save(mktmpl_cfg, os.path.join(output_dir, ".hydra", "mktmpl_config.yaml"))
+    # make dataset
+    _tdata: thd.TensorDict
+    vdata: thd.TensorDict
+    tstdata: thd.TensorDict
+    _tdata, vdata, tstdata = hd.utils.call(mktmpl_cfg.data)
+    n_covs: int = _tdata["xs"].shape[1]
+    n_labels: int = len(th.unique(_tdata["ys"]))
+    # split training data into two for classifier and afa
+    _tdata_shuffle_idxs: th.Tensor = th.load(
+        os.path.join(
+            mylib.utils.get_project_root_dir(), mktmpl_run_dir, "tdata_shuffle_idxs.pt"
+        )
+    )
+    tdata: thd.TensorDict
+    extdata: thd.TensorDict
+    tdata = _tdata[_tdata_shuffle_idxs[: len(_tdata) // 2]]
+    extdata = _tdata[_tdata_shuffle_idxs[len(_tdata) // 2 :]]
+    # make classifier
+    tclassifier: mymodels.classifiers.SubsetFeatureClassifier = hd.utils.instantiate(
+        mktmpl_cfg.tclassifier,
+        xs_train=extdata["xs"].numpy(),
+        ys_train=extdata["ys"].numpy(),
+    )
+    if isinstance(tclassifier, mymodels.classifiers.SubsetFeatureConcatClassifier):
+        tclassifier_p = os.path.join(
+            mylib.utils.get_project_root_dir(), mktmpl_run_dir, "tclassifier.pt"
+        )
+        if os.path.exists(tclassifier_p):
+            tclassifier.load_state_dict(th.load(tclassifier_p, map_location="cpu"))
+        else:
+            tclassifier.fit_(tmpls)
+    vclassifier: mymodels.classifiers.SubsetFeatureClassifier = tclassifier
+    if mktmpl_cfg.vclassifier is not None:
+        vclassifier = hd.utils.instantiate(
+            mktmpl_cfg.vclassifier,
+            xs_train=extdata["xs"].numpy(),
+            ys_train=extdata["ys"].numpy(),
+        )
+        if isinstance(vclassifier, mymodels.classifiers.SubsetFeatureConcatClassifier):
+            vclassifier_p = os.path.join(
+                mylib.utils.get_project_root_dir(), mktmpl_run_dir, "tclassifier.pt"
+            )
+            if os.path.exists(vclassifier_p):
+                vclassifier.load_state_dict(th.load(vclassifier_p, map_location="cpu"))
+            else:
+                vclassifier.fit_(tmpls)
+    os.makedirs(output_dir, exist_ok=True)
+    tfb_logger = plf_loggers.TensorBoardLogger(root_dir=output_dir, name="", version="")  # type: ignore
+    csv_logger = plf_loggers.CSVLogger(root_dir=output_dir, name="", version="")  # type: ignore
+    ckpt_p: str = os.path.join(tfb_logger.log_dir, "checkpoints")
+    plf: pl.Fabric = hd.utils.instantiate(cfg.plf, _partial_=True)(
+        loggers=[tfb_logger, csv_logger]
+    )
+    metrics_func = thm.MetricCollection(
+        {
+            "acc": thm.Accuracy(task="multiclass", num_classes=n_labels),
+            "precision": thm.Precision(task="multiclass", num_classes=n_labels),
+            "recall": thm.Recall(task="multiclass", num_classes=n_labels),
+            "f1-score": thm.F1Score(task="multiclass", num_classes=n_labels),
+            "auroc": thm.AUROC(task="multiclass", num_classes=n_labels),
+        }
+    )
+    # make nnet
+    nnet: th.nn.Module = hd.utils.instantiate(
+        cfg.nnet, in_features=2 * n_covs, out_features=len(tmpls)
+    )
+    # preapre fitting cost estimator
+    tstate = _TrainState(
+        nnet=nnet,
+        opt=hd.utils.instantiate(cfg.nnet_tcfg.opt, params=nnet.parameters()),
+        fit_itr=0,
+        opt_step=0,
+    )
+    stdata: thd.TensorDict = compile_selector_dataset(tdata=tdata, tpcomp=tpcomp)
+    # warmup nnet cost estimator
+    fit_nnet_selector(
+        tstate=tstate,
+        stdata=stdata,
+        init_fidx=mktmpl_cfg.init_fidx,
+        lmbda=mktmpl_cfg.lmbda,
+        tmpls=tmpls,
+        n_iter=cfg.nnet_tcfg.n_fit_iter,
+        bsz=cfg.nnet_tcfg.bsz,
+        plf=plf,
+        ckpt_p=ckpt_p,
+        save_ckpt_every_n_iter=cfg.nnet_tcfg.save_ckpt_every_n_iter,
+    )
+    plf.save(os.path.join(ckpt_p, "warmup_end.pt"), tstate)
+    # evaluate validation set performance with nnet after warmup
+    metrics_d = tafalib.utils.evaluate(
+        data=vdata,
+        classifier=vclassifier,
+        cost_est=lambda x: tafalib.functional.multi_output_nnet_cost_est(
+            x, nnet=nnet, lmbda=mktmpl_cfg.lmbda, tmpls=tmpls, device=plf.device
+        ),
+        init_fidx=mktmpl_cfg.init_fidx,
+        tmpls=tmpls,
+        metrics_func=metrics_func,
+        plf=plf,
+    )
+    plf.log_dict(mylib.utils.add_prefix_to_dict(metrics_d, "eval"))
+    # logger flush record and close
+    tfb_logger.finalize("success")
+    csv_logger.finalize("success")
+
+
+if __name__ == "__main__":
+
+    @hd.main(version_base=None)
+    def _main(cfg: MainConf):
+        logger = logging.getLogger(HydraConfig.get().job.name)
+        try:
+            main(cfg)
+        except Exception as e:
+            logger.error(e, exc_info=True, stack_info=True)
+            traceback.print_exception(e)
+
+    _main()
