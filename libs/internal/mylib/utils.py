465b8a53d935c7bb637c770bb609b2b20c7e4bf3
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-04-09 17:42:41 -0400

    docstring to mylib.utils

--- libs/internal/mylib/utils.py
+++ libs/internal/mylib/utils.py
@@ -7,8 +7,23 @@ from typing import Any, Optional
 def add_prefix_to_dict(
     d: dict[str, Any], prefix: str, sep: Optional[str] = "/"
 ) -> dict[str, Any]:
+    """add prefix to dictionary
+
+    Args:
+        d (dict[str, Any]): a dictionary
+        prefix (str): prefix to be prependes to each key
+        sep (Optional[str], optional): seperator between `prefix` and key. Defaults to "/".
+
+    Returns:
+        dict[str, Any]: new dictionary with `prefix` prepended to each key of `d`.
+    """
     return {f"{prefix}{sep}{k}": v for k, v in d.items()}
 
 
 def get_project_root_dir() -> str:
+    """get the absolute path to the project root
+
+    Returns:
+        str: absoluate path to project root.
+    """
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
