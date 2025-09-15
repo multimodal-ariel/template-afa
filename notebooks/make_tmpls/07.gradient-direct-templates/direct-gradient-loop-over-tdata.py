bff0478feef774113103b338f5844a6e74955f4e
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-09-15 14:34:10 -0400

    last evaluation use tmpls-pt

--- notebooks/make_tmpls/07.gradient-direct-templates/direct-gradient-loop-over-tdata.py
+++ notebooks/make_tmpls/07.gradient-direct-templates/direct-gradient-loop-over-tdata.py
@@ -541,41 +541,52 @@ metrics_d.update(
 print(pd.Series(metrics_d))
 
 # %%
-metrics_func.reset()
-snfobsd_l: list[int] = list()
-snfcomb_l: list[int] = list()
-for _data in vdata:
-    _pyhat, _fobsd_l, _fcomb = tafalib.utils.run_one_episode_all_obsd(
-        x=_data["xs"],
+if tmpls_pt is not None:
+    tpcomp_pt: thd.TensorDict = tafalib.utils.precomp_rwds_for_tmpls(
+        tmpls=tmpls_pt,
+        data=tdata,
         classifier=tclassifier,
-        cost_est=lambda x: tafalib.functional.knn_cost_est(
-            x,
-            lmbda=lmbda,
-            txs=tdata["xs"],
-            tcels=tpcomp["cels"],
-            tmpls=tmpls,
-            n_neighs=10,
-            p=2,
-        ),
-        init_fidx=init_fidx,
-        tmpls=tmpls,
+        lmbda=lmbda,
+        bsz=bsz,
         plf=plf,
     )
-    snfobsd_l.append(len(_fobsd_l))
-    snfcomb_l.append(len(_fcomb))
-    metrics_func.update(
-        _pyhat[None, :].to(device="cpu"), _data["ys"][None].to(device="cpu")
-    )
-metrics_d: dict[str, float] = {k: v.item() for k, v in metrics_func.compute().items()}
-metrics_func.reset()
-metrics_d.update(
-    {
-        "init_fidx": init_fidx,
-        "feature used & observed": th.mean(
-            th.as_tensor(snfobsd_l, dtype=th.float32)
-        ).item(),
+    metrics_func.reset()
+    snfobsd_l: list[int] = list()
+    snfcomb_l: list[int] = list()
+    for _data in vdata:
+        _pyhat, _fobsd_l, _fcomb = tafalib.utils.run_one_episode_all_obsd(
+            x=_data["xs"],
+            classifier=tclassifier,
+            cost_est=lambda x: tafalib.functional.knn_cost_est(
+                x,
+                lmbda=lmbda,
+                txs=tdata["xs"],
+                tcels=tpcomp_pt["cels"],
+                tmpls=tmpls_pt,
+                n_neighs=10,
+                p=2,
+            ),
+            init_fidx=init_fidx,
+            tmpls=tmpls,
+            plf=plf,
+        )
+        snfobsd_l.append(len(_fobsd_l))
+        snfcomb_l.append(len(_fcomb))
+        metrics_func.update(
+            _pyhat[None, :].to(device="cpu"), _data["ys"][None].to(device="cpu")
+        )
+    metrics_d: dict[str, float] = {
+        k: v.item() for k, v in metrics_func.compute().items()
     }
-)
-print(pd.Series(metrics_d))
+    metrics_func.reset()
+    metrics_d.update(
+        {
+            "init_fidx": init_fidx,
+            "feature used & observed": th.mean(
+                th.as_tensor(snfobsd_l, dtype=th.float32)
+            ).item(),
+        }
+    )
+    print(pd.Series(metrics_d))
 
 # %%
