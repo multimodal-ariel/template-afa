5c772213b8542136c442eb8d69d785bcc4befd60
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-11-21 01:50:32 -0500

    transform test time data

--- datasets/_python/mydatasets/aaco.py
+++ datasets/_python/mydatasets/aaco.py
@@ -18,16 +18,17 @@ def load_aaco_data(
         ),
         allow_pickle=True,
     )
-    xst: np.ndarray = data["train"][0]
+    xst: np.ndarray = data["train"][0].astype(np.float32)
     yst: np.ndarray = data["train"][1].flatten()
-    xsv: np.ndarray = data["valid"][0]
+    xsv: np.ndarray = data["valid"][0].astype(np.float32)
     ysv: np.ndarray = data["valid"][1].flatten()
-    xstst: np.ndarray = data["test"][0]
+    xstst: np.ndarray = data["test"][0].astype(np.float32)
     ystst: np.ndarray = data["test"][1].flatten()
     if to_normalize:
-        nmlr = skl_preproc.StandardScaler()
+        nmlr = skl_preproc.StandardScaler(copy=False)
         xst = nmlr.fit_transform(xst)
         xsv = nmlr.transform(xsv)
+        xstst = nmlr.transform(xstst)
     tdata = thd.TensorDict(
         {
             "xs": th.as_tensor(xst, dtype=th.float32),
