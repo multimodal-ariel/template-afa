df6377522350268cb720883e878706d9be14765f
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2026-02-08 16:05:00 -0500

    add main training script and implement evaluation with missing features

---
+++ experiments/make_template/train-w-missing.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+import logging
+import os
+import traceback
+from dataclasses import dataclass
+from typing import Any, Callable, Optional
+
+import hydra as hd
+import lightning as pl
+import lightning.fabric.loggers as plf_loggers
+import lightning.fabric.plugins.environments as plf_plugins_envs
+import mylib
+import mymodels
+import tafalib
+import tensordict as thd
+import torch as th
+import torchmetrics as thm
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import OmegaConf
+
+
+@dataclass
+class MainConf:
+    data: Any
+    tclassifier: Any
+    vclassifier: Optional[Any]
+    make_templates_fn: Any
+    init_fidx: int | Any | None
+    lmbda: float
+    n_neighs: int
+    missing_value: float
+    bsz: int
+    plf: Any
+
+
+OmegaConf.register_new_resolver(
+    name="get_cls", resolver=lambda cls: hd.utils.get_class(cls)
+)
+
+
+def main(cfg: MainConf):
+    # _delay_import()
+    output_dir: str = HydraConfig.get().runtime.output_dir
+    # make dataset
+    _tdata: thd.TensorDict
+    vdata: thd.TensorDict
+    tstdata: thd.TensorDict
+    _tdata, vdata, tstdata = hd.utils.call(cfg.data)
+    n_covs: int = _tdata["xs"].shape[1]
+    n_labels: int = len(th.unique(_tdata["ys"]))
+    # split training data into two for classifier and afa
+    _tdata_shuffle_idxs = th.randperm(
+        len(_tdata), generator=th.Generator().manual_seed(279)
+    )
+    tdata: thd.TensorDict
+    extdata: thd.TensorDict
+    tdata = _tdata[_tdata_shuffle_idxs[: len(_tdata) // 2]]
+    extdata = _tdata[_tdata_shuffle_idxs[len(_tdata) // 2 :]]
+    # configure logger and ckpt path
+    os.makedirs(output_dir, exist_ok=True)
+    tfb_logger = plf_loggers.TensorBoardLogger(root_dir=output_dir, name="", version="")  # type: ignore
+    csv_logger = plf_loggers.CSVLogger(root_dir=output_dir, name="", version="")  # type: ignore
+    plf: pl.Fabric = hd.utils.instantiate(cfg.plf, _partial_=True)(
+        loggers=[tfb_logger, csv_logger],
+        plugins=[plf_plugins_envs.LightningEnvironment()],  # type: ignore
+    )
+    # make classifier
+    tclassifier: mymodels.classifiers.SubsetFeatureClassifier = hd.utils.instantiate(
+        cfg.tclassifier, xs_train=extdata["xs"].numpy(), ys_train=extdata["ys"].numpy()
+    )
+    if isinstance(tclassifier, mymodels.classifiers.SubsetFeatureConcatXGBClassifier):
+        import sklearn.exceptions as skl_exceptions
+        import sklearn.utils as skl_utils
+
+        try:
+            [skl_utils.validation.check_is_fitted(_m) for _m in tclassifier._models]
+        except skl_exceptions.NotFittedError:
+            tclassifier.fit_(None)
+    vclassifier: mymodels.classifiers.SubsetFeatureClassifier = tclassifier
+    if cfg.vclassifier is not None:
+        vclassifier = hd.utils.instantiate(
+            cfg.vclassifier,
+            xs_train=extdata["xs"].numpy(),
+            ys_train=extdata["ys"].numpy(),
+        )
+    metrics_func = thm.MetricCollection(
+        {
+            "acc": thm.Accuracy(task="multiclass", num_classes=n_labels),
+            "precision": thm.Precision(task="multiclass", num_classes=n_labels),
+            "recall": thm.Recall(task="multiclass", num_classes=n_labels),
+            "f1-score": thm.F1Score(task="multiclass", num_classes=n_labels),
+            "auroc": thm.AUROC(task="multiclass", num_classes=n_labels),
+        }
+    )
+    # add eval validation envirionment kwags
+    # TODO modify tafalib.makers.templates direclty to provide common interface
+    _mktmpl_fn_kwargs: dict[str, Any] = dict()
+    if str.split(cfg.make_templates_fn._target_, ".")[-1] in (
+        "make_templates_direct_greedy_with_undo",
+        "make_templates_direct_minibatch_gradient",
+    ):
+        _mktmpl_fn_kwargs.update(
+            {
+                "n_neighs": cfg.n_neighs,
+                "vdata": vdata,
+                "vclassifier": vclassifier,
+                "metrics_func": metrics_func,
+            }
+        )
+    elif str.split(cfg.make_templates_fn._target_, ".")[-1] in (
+        "make_templates_cyclic_greedy_with_undo",
+    ):
+        _mktmpl_fn_kwargs.update(
+            {
+                "feature_opt_cycler": hd.utils.instantiate(
+                    cfg.make_templates_fn.feature_opt_cycler, _partial_=True
+                )(tdata=tdata),
+                "n_neighs": cfg.n_neighs,
+                "vdata": vdata,
+                "vclassifier": vclassifier,
+                "metrics_func": metrics_func,
+            }
+        )
+        del cfg.make_templates_fn.feature_opt_cycler
+    # make templates
+    make_templates_fn: Callable = hd.utils.instantiate(
+        cfg.make_templates_fn, _partial_=True
+    )
+    init_fidx: int | Callable | None = cfg.init_fidx
+    if cfg.init_fidx is not None and not isinstance(cfg.init_fidx, int):
+        init_fidx = hd.utils.instantiate(cfg.init_fidx, _partial_=True)
+    tmpls: th.Tensor = make_templates_fn(
+        tdata=tdata,
+        classifier=tclassifier,
+        init_fidx=init_fidx,
+        lmbda=cfg.lmbda,
+        bsz=cfg.bsz,
+        plf=plf,
+        **_mktmpl_fn_kwargs,
+    )
+    # save templates
+    th.save(_tdata_shuffle_idxs, os.path.join(output_dir, "tdata_shuffle_idxs.pt"))
+    th.save(tmpls, os.path.join(output_dir, "tmpls.pt"))
+    # save training classifier
+    if len(tclassifier.state_dict()) != 0:
+        th.save(tclassifier.state_dict(), os.path.join(output_dir, "tclassifier.pt"))
+    # evaluate validation set performance
+    tpcomp: thd.TensorDict = tafalib.utils.precomp_rwds_for_tmpls(
+        tmpls=tmpls,
+        data=tdata,
+        classifier=tclassifier,
+        lmbda=cfg.lmbda,
+        bsz=cfg.bsz,
+        plf=plf,
+    )
+    th.save(tpcomp, os.path.join(output_dir, "tpcomp.pt"))
+    metrics_d: dict[str, float] = tafalib.utils.evaluate_with_missing_feature(
+        data=vdata,
+        classifier=vclassifier,
+        cost_est=lambda x: tafalib.functional.knn_cost_est(
+            x,
+            lmbda=cfg.lmbda,
+            txs=tdata["xs"],
+            tcels=tpcomp["cels"],
+            tmpls=tmpls,
+            n_neighs=cfg.n_neighs,
+            p=2,
+        ),
+        init_fidx=(
+            init_fidx
+            if isinstance(init_fidx, int)
+            else int(th.argwhere(th.all(tmpls.to(dtype=th.bool), dim=0).flatten())[0])
+        ),
+        tmpls=tmpls,
+        missing_value=cfg.missing_value,
+        lmbda=cfg.lmbda,
+        metrics_func=metrics_func,
+        plf=plf,
+    )
+    plf.log_dict(mylib.utils.add_prefix_to_dict(metrics_d, "eval"))
+    # save validation time classifier
+    # if it's neither the one used in training time
+    # nor has empty state_dict
+    if vclassifier is not tclassifier and len(vclassifier.state_dict()) != 0:
+        th.save(vclassifier.state_dict(), os.path.join(output_dir, "vclassifier.pt"))
+    # logger flush record and close
+    tfb_logger.finalize("success")
+    csv_logger.finalize("success")
+
+
+if __name__ == "__main__":
+
+    @hd.main(version_base=None)
+    def _main(cfg: MainConf):
+        logger = logging.getLogger(HydraConfig.get().job.name)
+        try:
+            main(cfg)
+        except Exception as e:
+            logger.error(e, exc_info=True, stack_info=True)
+            traceback.print_exception(e)
+
+    _main()
--- libs/internal/tafalib/utils.py
+++ libs/internal/tafalib/utils.py
@@ -16,56 +16,6 @@ def feature_masks_to_feature_combs(tmpls: th.Tensor):
     return [tuple(th.argwhere(_tmpl == 1).flatten().tolist()) for _tmpl in tmpls]
 
 
-# NOTE high memory usage
-# @th.no_grad()
-# def precomp_rwds_for_tmpls(
-#     tmpls: th.Tensor,
-#     data: thd.TensorDict,
-#     classifier: mymodels.classifiers.SubsetFeatureClassifier,
-#     lmbda: float,
-#     bsz: int,
-# ) -> thd.TensorDict:
-#     txs: th.Tensor = data["xs"]
-#     tys: th.Tensor = data["ys"]
-#     n_cands: int = len(tmpls)
-#     n_labels: int = len(th.unique(tys))
-#     # (n_data,  n_cands, n_labels)
-#     pyhats: th.Tensor = th.empty((len(txs), n_cands, n_labels), dtype=th.float32)
-#     # (n_data,  n_cands)
-#     cels: th.Tensor = th.empty((len(txs), n_cands), dtype=th.float32)
-#     rwds: th.Tensor = th.empty_like(cels)
-#     pbar = tqdm.tqdm(
-#         th.split(th.arange(0, len(txs), dtype=th.long), bsz),
-#         desc="precomp candidates",
-#         leave=False,
-#         dynamic_ncols=True,
-#     )
-#     for _btidxs in pbar:
-#         _bsz: int = len(_btidxs)
-#         # (_bsz * n_cands, n_covs)
-#         _bctxs: th.Tensor = txs[_btidxs, None, :].expand(-1, n_cands, -1).flatten(0, 1)
-#         _bacts: th.Tensor = tmpls[None, :, :].expand(_bsz, -1, -1).flatten(0, 1)
-#         _bctxs = _bctxs * _bacts
-#         # (_bsz * n_cands, n_labels)
-#         _bpyhats: th.Tensor = classifier.predict_proba(_bctxs, _bacts)
-#         _blyhats: th.Tensor = torch.distributions.utils.probs_to_logits(_bpyhats)
-#         # (_bsz * n_cands)
-#         _btys: th.Tensor = tys[_btidxs, None].expand(-1, n_cands).flatten(0, 1)
-#         _bcels: th.Tensor = th.nn.functional.cross_entropy(
-#             _blyhats, _btys, reduction="none"
-#         )
-#         _brwds: th.Tensor = -_bcels - lmbda * th.sum(_bacts, dim=1)
-#         pyhats[_btidxs] = _bpyhats.unflatten(0, (_bsz, n_cands))
-#         cels[_btidxs] = _bcels.unflatten(0, (_bsz, n_cands))
-#         rwds[_btidxs] = _brwds.unflatten(0, (_bsz, n_cands))
-#     pbar.close()
-#     # turn into tensordict
-#     tpcomp = thd.TensorDict(
-#         {"pyhats": pyhats, "cels": cels, "rwds": rwds}
-#     ).auto_batch_size_(1)
-#     return tpcomp
-
-
 @th.no_grad()
 def precomp_rwds_for_tmpls(
     tmpls: th.Tensor,
@@ -357,3 +307,144 @@ def predict(
         fms[_i, _fobsd_l] = 1
         fobsds_l.append(_fobsd_l)
     return pyhats, oms, fms, fobsds_l
+
+
+@th.no_grad()
+def run_one_episode_all_obsd_with_missing_feature(
+    x: th.Tensor,
+    classifier: mymodels.classifiers.SubsetFeatureClassifier,
+    cost_est: Callable[[th.Tensor], th.Tensor],
+    init_fidx: int,
+    tmpls: th.Tensor,
+    missing_value: float,
+    plf: pl.Fabric,
+) -> tuple[th.Tensor, list[int], tuple[int, ...]]:
+    """run one episode using **all** features acquired
+
+    Args:
+        x (th.Tensor): (n_covs, ) instance of interest
+        classifier (mymodels.classifiers.SubsetFeatureClassifier): subset feature classifier used to make prediction
+        cost_est (Callable[[th.Tensor], th.Tensor]): a cost estimator mapping a tensor of shape (n, 2 * n_covs) to a tensor of (n, n_tmpls)
+        init_fidx (int): initial feature indices
+        tmpls (th.Tensor): (n_tmpls, n_covs)
+        plf (pl.Fabric): lightning fabric instance
+
+    Returns::
+        th.Tensor: (n_labels, ) predicted class probability
+        list[int]: (n_feats_obsd, ) a list of feature indices in the order of acquisition.
+        tuple[int, ...]]: (n_feats_obsd, ) a tuple of feature indices acquired; does NOT preseve acquisition order
+    """
+    classifier.eval().to(device=plf.device)
+    if isinstance(cost_est, th.nn.Module):
+        cost_est.eval().to(device=plf.device)
+    x = x.to(device=plf.device)
+    # handle the case where certain feature is completely missing in given dataset
+    tmpls = th.where(
+        (x == missing_value)[None, :].expand((len(tmpls), -1)), 0, tmpls
+    ).to(device=plf.device)
+    # in case initial feature is not observed
+    if th.any(tmpls[:, init_fidx]) == 0:
+        _feature_avail: list[int] = (
+            th.argwhere(th.sum(tmpls, dim=0) > 0).flatten().tolist()
+        )
+        _ridx: int = int(th.randint(0, len(_feature_avail), ()).item())
+        init_fidx = _feature_avail[_ridx]
+    # rollout current instance
+    fobsd_l: list[int] = [init_fidx]
+    for _ in itrtls.count():
+        # make feature bit mask
+        _fm: th.Tensor = th.zeros_like(x)
+        _fm[fobsd_l] = 1
+        # forward prop. cost est.
+        # (1, 2 * n_covs)
+        _inps: th.Tensor = th.cat((x * _fm, _fm))[None, :]
+        # (1, n_tmpls)
+        _costs: th.Tensor = cost_est(_inps)
+        _tmpl_idx: int = int(th.argmin(_costs[0]).item())
+        _fm_avail: th.Tensor = th.maximum(
+            tmpls[_tmpl_idx] - _fm, th.as_tensor(0.0, device=plf.device)
+        )
+        if th.sum(_fm_avail) == 0:
+            break
+        fobsd_l.append(int(th.argmax(_fm_avail).item()))
+    fcomb = tuple(sorted(fobsd_l))
+    acts: th.Tensor = th.zeros((1, x.shape[0]), dtype=th.long, device=x.device)
+    acts[0, fobsd_l] = 1
+    pyhats: th.Tensor = classifier.predict_proba(x[None, :] * acts, acts)
+    return pyhats[0], fobsd_l, fcomb
+
+
+def evaluate_with_missing_feature(
+    data: thd.TensorDict,
+    classifier: mymodels.classifiers.SubsetFeatureClassifier,
+    cost_est: Callable[[th.Tensor], th.Tensor],
+    init_fidx: int,
+    tmpls: th.Tensor,
+    missing_value: float,
+    lmbda: float,
+    metrics_func: thm.MetricCollection,
+    plf: pl.Fabric,
+) -> dict[str, float]:
+    """compute metrics through rolling out policy over a dataset
+
+    Args:
+        data (thd.TensorDict): (n, ) dataset of interest; must contain `xs` and `ys`
+        classifier (mymodels.classifiers.SubsetFeatureClassifier): subset feature classifier used to make prediction
+        cost_est (Callable[[th.Tensor], th.Tensor]): cost estimator
+        init_fidx (int): initial feature index
+        tmpls (th.Tensor): (n_tmpls, n_covs) collection of templates
+        lmbda (float): penalty for acquiring more features
+        metrics_func (thm.MetricCollection): a collection of metrics of interests
+        plf (pl.Fabric): plf instance for prediction
+
+    Returns:
+        dict[str, float]: a dictionary of metrics
+    """
+    snfobsd_l: list[int] = list()
+    snfcomb_l: list[int] = list()
+    pyhats_l: list[th.Tensor] = list()
+    fms: th.Tensor = th.zeros((len(data), data["xs"].shape[1]), dtype=th.long)
+    metrics_func.reset()
+    start_time_ns: int = time.time_ns()
+    for _i, _data in tqdm.tqdm(
+        enumerate(data), desc="eval", total=len(data), dynamic_ncols=True, leave=False
+    ):
+        _pyhat, _fobsd_l, _fcomb = run_one_episode_all_obsd_with_missing_feature(
+            x=_data["xs"],
+            classifier=classifier,
+            cost_est=cost_est,
+            init_fidx=init_fidx,
+            tmpls=tmpls,
+            missing_value=missing_value,
+            plf=plf,
+        )
+        snfobsd_l.append(len(_fobsd_l))
+        snfcomb_l.append(len(_fcomb))
+        pyhats_l.append(_pyhat[None, :].to(device="cpu"))
+        fms[_i, _fcomb] = 1
+        metrics_func.update(
+            _pyhat[None, :].to(device="cpu"), _data["ys"][None].to(device="cpu")
+        )
+    end_time_ns: int = time.time_ns()
+    pyhats: th.Tensor = th.cat(pyhats_l, dim=0)
+    cels: th.Tensor = th.nn.functional.cross_entropy(
+        pyhats, data["ys"], reduction="none"
+    )
+    rwds: th.Tensor = -cels - lmbda * th.sum(fms, dim=1)
+    metrics_d: dict[str, float] = {
+        k: v.item() for k, v in metrics_func.compute().items()
+    }
+    metrics_func.reset()
+    metrics_d.update(
+        {
+            "init_fidx": init_fidx,
+            "reward": th.mean(rwds, dim=0).item(),
+            "feature observed": th.mean(
+                th.as_tensor(snfobsd_l, dtype=th.float32)
+            ).item(),
+            "feature used": th.mean(th.as_tensor(snfcomb_l, dtype=th.float32)).item(),
+            "inference_time_ns": end_time_ns - start_time_ns,
+            "avg_pred_time_ns": (end_time_ns - start_time_ns) / len(data),
+        }
+    )
+    return metrics_d
